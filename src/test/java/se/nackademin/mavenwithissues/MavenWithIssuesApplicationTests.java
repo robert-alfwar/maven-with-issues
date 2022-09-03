12b93e1e300b91c4881e4bf150c62c5af8ff2dee
Robert Westin <7487277-robert-alfwar@users.noreply.gitlab.com>
2022-09-03 17:54:06 +0200

    add spring boot example

---
+++ src/test/java/se/nackademin/mavenwithissues/MavenWithIssuesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.nackademin.mavenwithissues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenWithIssuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
